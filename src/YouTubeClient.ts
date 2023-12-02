208369f00fe88db1fc84757f4a0f790811027127
Benjamin Hutchings <benjamin@blhutchings.com>
2023-12-02 15:13:27 -0400

    Swapped to axios

--- src/YouTubeClient.ts
+++ src/YouTubeClient.ts
@@ -1,38 +1,45 @@
-import ky, { KyInstance } from "ky";
+import axios, { AxiosInstance } from "axios";
 import { Schema$Game, SearchParams$Game } from "./types/Game.js";
 import { SearchParams$Video, Schema$Video } from "./types/Video.js";
 import { SearchParams$Gaming, Schema$Gaming } from "./types/Gaming.js";
 import { SearchParams$GameTitle, Schema$GameTitle } from "./types/GameTitle.js";
 
+
 export interface YouTubeClientOptions {
 	host: string,
 	port: number,
-	https: boolean
+	https?: boolean
 }
 
 export class YouTubeClient {
-	private httpClient: KyInstance
+	private httpClient: AxiosInstance
 	private options: YouTubeClientOptions
 
 	constructor(options?: YouTubeClientOptions) {
-		this.options = options ?? { host: "localhost", port: 3000, https: false }
-		this.httpClient = ky.extend({ prefixUrl: `http${this.options.https ? 's' : ''}://${this.options.host}:${this.options.port}` })
+		this.options = {
+			host: options?.host ?? "localhost",
+			port: options?.port ?? 3000,
+			https: options?.https ?? false
+		}
+		this.httpClient = axios.create({
+			baseURL: `http${this.options.https ? 's' : ''}://${this.options.host}:${this.options.port}/`
+		})
 	}
 
 	video(searchParams: SearchParams$Video): Promise<Schema$Video> {
-		return this.httpClient.post("/video", { json: searchParams }).json()
+		return this.httpClient.post("/video", { json: searchParams })
 	}
 
 	game(searchParams: SearchParams$Game): Promise<Schema$Game> {
-		return this.httpClient.post("/game", { json: searchParams }).json()
+		return this.httpClient.post("/game", { json: searchParams })
 	}
 
 	gaming(searchParams: SearchParams$Gaming): Promise<Schema$Gaming> {
-		return this.httpClient.post("/gaming", { json: searchParams }).json()
+		return this.httpClient.post("/gaming", { json: searchParams })
 	}
 
 	gameTitle(searchParams: SearchParams$GameTitle): Promise<Schema$GameTitle> {
-		return this.httpClient.post("/gametitle", { json: searchParams }).json()
+		return this.httpClient.post("/gametitle", { json: searchParams })
 	}
 
 }
