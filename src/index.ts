1955aaf488c652ba15fbb07a01926c5edbc9c6cb
Benjamin Hutchings <benjamin@blhutchings.com>
2023-12-23 15:01:57 -0400

    Added more export types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
+export { Schema$CompactRenderer } from "./types/components/CompactRenderer.js"
+export { Schema$CompactRendererMovie } from "./types/components/CompactRendererMovie.js"
+export { Schema$CompactRendererPlaylist } from "./types/components/CompactRendererPlaylist.js"
+export { Schema$CompactRendererVideo } from "./types/components/CompactRendererVideo.js"
+export { Schema$RichItemRenderer } from "./types/components/RichItemRenderer.js"
 
-export { SearchParams$Account, Schema$Account } from "./types/Account.js"
-export { SearchParams$Game, Schema$Game } from "./types/Game.js"
-export { SearchParams$Gaming, Schema$Gaming } from "./types/Gaming.js"
+export { Schema$Thumbnail, Schema$ThumbnailDetails } from "./types/Thumbnail.js"
+
+export {
+	SearchParams$Account, Schema$Account,
+	Schema$AccountSnippet,
+	Schema$AccountAbout,
+	Schema$AccountLive,
+	Schema$AccountVideos
+} from "./types/Account.js"
+
+export {
+	SearchParams$Game, Schema$Game,
+	Schema$GameSnippet,
+	Schema$GameHome,
+	Schema$GameLive,
+	Schema$GameRecent,
+	Schema$GameOfficial,
+	Schema$GameAbout,
+	Schema$GameCard
+} from "./types/Game.js"
+
+export {
+	SearchParams$Gaming, Schema$Gaming,
+	Schema$GamingSnippet,
+	Schema$GamingLive,
+	Schema$GamingTrending
+} from "./types/Gaming.js"
+
 export { SearchParams$GameTitle, Schema$GameTitle } from "./types/GameTitle.js"
+
 export { YouTubeClient, YouTubeClientOptions } from "./YouTubeClient.js"
+
